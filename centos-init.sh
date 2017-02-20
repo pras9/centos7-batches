c84fe113ff52923b2006bd23fbadd297851acefc
pras <me@prasun.net>
2017-02-20 14:55:02 +0530

    postgresql-setup initdb

--- centos-init.sh
+++ centos-init.sh
@@ -10,6 +10,8 @@ yum install -y gcc-c++ make
 yum install httpd -y
 yum install mariadb-server mariadb -y
 yum install postgresql-server postgresql-contrib -y
+postgresql-setup initdb
+# for installation of postgres to check: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-postgresql-on-centos-7 
 yum install php php-cli php-common php-mysql php-pgsql php_pdo-pgsql php-dba php-pear php-enchant php-gd php-imap -y
 curl --silent --location https://rpm.nodesource.com/setup | bash -
 yum install -y nodejs npm
